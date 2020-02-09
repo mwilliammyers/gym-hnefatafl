f9b896b474f82c003bf0656471a824d5ac9d2c00
William Myers <mwilliammyers@gmail.com>
2020-02-08 17:40:30 -0700

    Add packages key to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
-setup(name='gym_hnefatafl',
-      version='0.0.1',
-      install_requires=['gym', 'numpy', "scipy"]  # And any other dependencies foo needs
-      )
+setup(
+    name="gym_hnefatafl",
+    version="0.0.1",
+    packages=find_namespace_packages(),
+    install_requires=["gym", "numpy", "scipy"],
+)

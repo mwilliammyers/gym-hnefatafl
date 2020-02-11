7247b00b74a6e48bca791dcb967367ef59d61d8e
William Myers <mwilliammyers@gmail.com>
2020-02-10 20:03:45 -0700

    Register additional envs

--- gym_hnefatafl/__init__.py
+++ gym_hnefatafl/__init__.py
@@ -1,6 +1,23 @@
 from gym.envs.registration import register
 
 register(
-    id='hnefatafl-v0',
-    entry_point='gym_hnefatafl.envs:HnefataflEnv',
+    id="hnefatafl-v0", entry_point="gym_hnefatafl.envs:HnefataflEnv",
+)
+
+register(
+    id="hnefatafl-7x7-v0",
+    entry_point="gym_hnefatafl.envs:HnefataflEnv",
+    kwargs={"size": 7},
+)
+
+register(
+    id="hnefatafl-9x9-v0",
+    entry_point="gym_hnefatafl.envs:HnefataflEnv",
+    kwargs={"size": 9},
+)
+
+register(
+    id="hnefatafl-11x11-v0",
+    entry_point="gym_hnefatafl.envs:HnefataflEnv",
+    kwargs={"size": 11},
 )

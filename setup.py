905a846dab7bd1790e9bde88a2fbf64b1bb78372
William Myers <mwilliammyers@gmail.com>
2020-02-10 20:04:34 -0700

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="gym_hnefatafl",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_namespace_packages(),
     install_requires=["gym", "numpy", "scipy"],
 )
